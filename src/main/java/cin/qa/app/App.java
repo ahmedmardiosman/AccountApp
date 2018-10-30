42aa41bf90f79bb9d94596f5c4dd51089d0d00b3
ahmedmardiosman <ahmedmardiosman@gmail.com>
2018-10-30 11:58:08 +0000

    Final Commit

--- src/main/java/cin/qa/app/App.java
+++ src/main/java/cin/qa/app/App.java
@@ -19,11 +19,18 @@ public class App {
 		s.addAccount(a2);
 		
 		
-		s.deleteAccount(102);
+	//	s.deleteAccount(102);
 		
 		System.out.println(s.getAccounts());
 		
+Accounts a3 = new Accounts (103, "Adonay" , "G" , "12345678");
 		
+		Accounts a4 = new Accounts (104, "Adil" , "O" , "87654321");
+		
+		s.addAccount(a3);
+		s.addAccount(a4);
+		
+		System.out.println(s.getAccounts());
 		
 	}
 
