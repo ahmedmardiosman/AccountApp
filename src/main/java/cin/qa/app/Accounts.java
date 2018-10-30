c10b287a900f33b3a4a266f6c5ccdaaa8414cf89
ahmedmardiosman <ahmedmardiosman@gmail.com>
2018-10-30 10:30:56 +0000

    Third Commit

--- src/main/java/cin/qa/app/Accounts.java
+++ src/main/java/cin/qa/app/Accounts.java
@@ -2,4 +2,57 @@ package cin.qa.app;
 
 public class Accounts {
 
+	private int id;
+	private String firstName;
+	private String lastName;
+	private String accountNumber;
+
+	public Accounts(int id, String firstName, String lastName, String accountNumber) {
+
+		this.setId(id);
+		this.setFirstName(firstName);
+		this.setLastName(lastName);
+		this.setAccountNumber(accountNumber);
+
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+
+	
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return "Accounts [id : " + id + ", firstName : " + firstName + ", lastName : " + lastName + ", accountNumber : "
+				+ accountNumber + "]";
+	}
+
 }
--- src/main/java/cin/qa/app/app.java
+++ src/main/java/cin/qa/app/app.java
@@ -5,7 +5,23 @@ public class app {
 	
 	public static void main(String [] Args) {
 		
-		System.out.println("Hello worlds");
+		
+		Service s = new Service();
+		
+		Accounts a1 = new Accounts (101, "Ahmed" , "Osman" , "12345678");
+		
+		Accounts a2 = new Accounts (102, "Harrison" , "Groome" , "87654321");
+		
+		s.addAccount(a1);
+		s.addAccount(a2);
+		
+		
+		s.deleteAccount(102);
+		
+		System.out.println(s.getAccounts());
+		
+		
+		
 	}
 
 }
