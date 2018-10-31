bd42e0fe5038f4def13f6c2049279da0a26ea9d9
ahmedmardiosman <ahmedmardiosman@gmail.com>
2018-10-31 10:02:59 +0000

    Second Test Commit

--- src/main/java/cin/qa/app/Service.java
+++ src/main/java/cin/qa/app/Service.java
@@ -7,6 +7,21 @@ public class Service {
 
 	static HashMap<Integer, Accounts> accounts = new HashMap<>();
 
+	public static int duplicatedAccounts(String name) {
+
+		int accs = 0;
+
+		for (int key : accounts.keySet()) {
+
+			if (accounts.get(key).getFirstName().equals(name)) {
+
+				accs++;
+			}
+		}
+		return accs;
+
+	}
+
 	public static void addAccount(Accounts acc) {
 		accounts.put(acc.getId(), acc);
 
--- src/test/java/accountapp/AccountAppTest.java
+++ src/test/java/accountapp/AccountAppTest.java
@@ -1,25 +1,41 @@
 package accountapp;
 
-
-
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
-
+import cin.qa.app.Accounts;
+import cin.qa.app.Service;
 
 public class AccountAppTest {
+
+	@Before
+	public void beforeFunction() {
+
+		Accounts a1 = new Accounts(101, "Ahmed", "Osman", "12345678");
+
+		Accounts a2 = new Accounts(102, "Harrison", "Groome", "87654321");
+
+		Accounts a3 = new Accounts(103, "Adonay", "G", "45342578");
+
+		Accounts a4 = new Accounts(104, "Ahmed", "O", "87676521");
+
+		Service s = new Service();
+
+		s.addAccount(a1);
+		s.addAccount(a2);
+		s.addAccount(a3);
+		s.addAccount(a4);
+
+	}
 
 	@Test
 	public void test() {
 
+		int actual = Service.duplicatedAccounts("Ahmed");
 
-		
-		
-		
-		
-		Assert.assertEquals(2, 2);
-		
-		
+		Assert.assertEquals(2, actual);
+
 	}
 
 }
