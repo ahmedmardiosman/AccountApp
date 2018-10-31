92519e96fae4c3c0e54ec468b21b6eac93e6053d
ahmedmardiosman <ahmedmardiosman@gmail.com>
2018-10-31 15:38:23 +0000

    Third Commit

--- src/main/java/com/qa/rest/AccountsEndpoint.java
+++ src/main/java/com/qa/rest/AccountsEndpoint.java
@@ -1,19 +1,13 @@
 package com.qa.rest;
 
 import javax.inject.Inject;
-import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-
-import com.qa.business.service.MovieService;
 
 import cin.qa.app.AccountsService;
 
-import org.apache.log4j.Logger;
+
 
 @Path("/accounts")
 public class AccountsEndpoint {
